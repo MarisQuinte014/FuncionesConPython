fdb8c871c9c3363a23933b3ff37c56f7ff7e44b8
Mariana Quintero <marianaquintero@fisapay.com>
2022-09-30 20:42:22 -0500

    corrigiendo la propiedad

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,4 +1,4 @@
 lista = [12,2,32,1,123,3,23,2,-9]
 
-clasificacionNumeros = list(filter(lambda x: x > 10, lista))
+clasificacionNumeros = list(filter(lambda numero: numero > 10, lista))
 print(clasificacionNumeros)
